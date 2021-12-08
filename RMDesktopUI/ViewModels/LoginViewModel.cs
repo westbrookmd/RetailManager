086555357cd949974801d1da865187f86d0a1653
westbrookmd <westbrookmd@email.wofford.edu>
2021-12-07 23:48:04 -0500

    Error Handling for Login Form
    Added error handling to the loginview and loginviewmodel via WPF binding (with app.xaml)

--- RMDesktopUI/ViewModels/LoginViewModel.cs
+++ RMDesktopUI/ViewModels/LoginViewModel.cs
@@ -43,6 +43,35 @@ namespace RMDesktopUI.ViewModels
             }
         }
 
+
+        public bool IsErrorVisible
+        {
+            get
+            {
+                bool output = false;
+                if (ErrorMessage?.Length > 0)
+                {
+                    output = true;
+                }
+                return output;
+            }
+        }
+
+        private string _errorMessage;
+
+        public string ErrorMessage
+        {
+            get { return _errorMessage; }
+            set 
+            {
+                _errorMessage = value;
+                NotifyOfPropertyChange(() => IsErrorVisible);
+                NotifyOfPropertyChange(() => ErrorMessage);
+            }
+        }
+
+
+
         public bool CanLogIn
         {
             get
@@ -63,11 +92,12 @@ namespace RMDesktopUI.ViewModels
         {
             try
             {
+                ErrorMessage = "";
                 var result = await _apiHelper.Authenticate(UserName, Password);
             }
             catch (Exception ex)
             {
-                Console.WriteLine(ex.Message);
+                ErrorMessage = ex.Message;
             }
         }
 

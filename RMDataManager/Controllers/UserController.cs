2a08adadb6bb429aaddbe7e4d1fe773841213f53
westbrookmd <westbrookmd@email.wofford.edu>
2021-12-11 00:34:51 -0500

    Created User Model & Connected API Endpoint
    Using Dapper to hook up SQL. User Model currently returns an array of users even though there isn't a use case for this yet. Need to wire up to WPF application now that API is functioning.

---
+++ RMDataManager/Controllers/UserController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNet.Identity;
+using RMDataManager.Library.DataAccess;
+using RMDataManager.Library.Internal.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace RMDataManager.Controllers
+{
+    [Authorize]
+    public class UserController : ApiController
+    {
+        public List<UserModel> GetById()
+        {
+            //current logged in user's id
+            string userId = RequestContext.Principal.Identity.GetUserId();
+            UserData data = new UserData();
+
+            return data.GetUserById(userId);
+        }
+    }
+}
